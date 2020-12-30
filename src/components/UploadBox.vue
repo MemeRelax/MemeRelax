4745d6d7357fbaebb371cdbba057844c7a593673
Magda Kokot <magda.kok7@gmail.com>
2020-12-30 10:16:57 +0000

    start working on upload box

--- src/components/UploadBox.vue
+++ src/components/UploadBox.vue
@@ -2,6 +2,32 @@
   <div class="c-upload-box">
     <div class="c-upload-box__container">
       <h1 class="heading-h1">Upload your meme</h1>
+      <form
+        class="c-upload-box__form"
+        method="post"
+        action=""
+        enctype="multipart/form-data"
+      >
+        <div class="c-upload-box__input">
+          <input
+            class="c-upload-box__file"
+            type="file"
+            name="files[]"
+            id="file"
+            data-multiple-caption="{count} files selected"
+            multiple
+          />
+          <label for="file"
+            ><strong>Choose a file</strong
+            ><span class="c-upload-box__dragndrop"> or drag it here</span
+            >.</label
+          >
+          <button class="c-upload-box__button" type="submit">Upload</button>
+        </div>
+        <div class="c-upload-box__uploading">Uploading…</div>
+        <div class="c-upload-box__success">Done!</div>
+        <div class="c-upload-box__error">Error! <span></span>.</div>
+      </form>
     </div>
   </div>
 </template>
@@ -19,5 +45,12 @@ export default {
 
 .c-upload-box__container {
   @extend .container;
+}
+
+.c-upload-box__dragndrop,
+.c-upload-box__uploading,
+.c-upload-box__success,
+.c-upload-box__error {
+  display: none;
 }
 </style>
