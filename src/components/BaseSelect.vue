c9b79e7c82a76896d5d42c15ba8a322b3427c157
Magda Kokot <magda.kokot@mandogroup.com>
2021-01-14 15:43:38 +0000

    create base select component

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,41 @@
+<template>
+  <label v-if="label" :for="$attrs.id">{{ label }}</label>
+  <select
+    class="b-select__select"
+    v-bind="{
+      ...$attrs,
+      onChange: ($event) => {
+        $emit('update:modelValue', $event.target.value);
+      },
+    }"
+    :value="modelValue"
+  >
+    <option
+      v-for="option in options"
+      :value="option"
+      :key="option"
+      :selected="option === modelValue"
+    ></option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
