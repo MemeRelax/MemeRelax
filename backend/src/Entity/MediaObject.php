5f790084e46f4a3c496bddc02efc64464002aac1
Kamil Kokot <kamil@kokot.me>
2021-01-15 21:40:43 +0100

    Move Serializer configuration to XML

--- backend/src/Entity/MediaObject.php
+++ backend/src/Entity/MediaObject.php
@@ -4,24 +4,16 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-use App\Controller\CreateMediaObjectAction;
-use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Uid\Uuid;
 
 class MediaObject
 {
-    /**
-     * @Groups({"media_object_read"})
-     */
     public ?string $contentUrl = null;
 
     private Uuid $id;
 
     private string $filePath;
 
-    /**
-     * @Groups({"media_object_read"})
-     */
     private string $blurhash;
 
     public function __construct(string $filePath, string $blurhash)
