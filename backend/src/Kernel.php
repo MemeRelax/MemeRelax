322e9da8a36989eab8845ac81092935f633b3989
Kamil Kokot <kamil@kokot.me>
2021-01-15 16:38:00 +0100

    Apply coding standard fixes

--- backend/src/Kernel.php
+++ backend/src/Kernel.php
@@ -1,11 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+use function dirname;
+use function is_file;
 
 class Kernel extends BaseKernel
 {
@@ -14,27 +19,35 @@ class Kernel extends BaseKernel
     protected function configureContainer(ContainerConfigurator $container): void
     {
         $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
+        $container->import('../config/{packages}/' . $this->environment . '/*.yaml');
 
-        if (is_file(\dirname(__DIR__).'/config/services.yaml')) {
+        if (is_file(dirname(__DIR__) . '/config/services.yaml')) {
             $container->import('../config/services.yaml');
-            $container->import('../config/{services}_'.$this->environment.'.yaml');
-        } elseif (is_file($path = \dirname(__DIR__).'/config/services.php')) {
-            /** @psalm-suppress UnresolvableInclude */
-            (require $path)($container->withPath($path), $this);
+            $container->import('../config/{services}_' . $this->environment . '.yaml');
+        } else {
+            $path = dirname(__DIR__) . '/config/services.php';
+
+            if (is_file($path)) {
+                /** @psalm-suppress UnresolvableInclude */
+                (require $path)($container->withPath($path), $this);
+            }
         }
     }
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
+        $routes->import('../config/{routes}/' . $this->environment . '/*.yaml');
         $routes->import('../config/{routes}/*.yaml');
 
-        if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
+        if (is_file(dirname(__DIR__) . '/config/routes.yaml')) {
             $routes->import('../config/routes.yaml');
-        } elseif (is_file($path = \dirname(__DIR__).'/config/routes.php')) {
-            /** @psalm-suppress UnresolvableInclude */
-            (require $path)($routes->withPath($path), $this);
+        } else {
+            $path = dirname(__DIR__) . '/config/routes.php';
+
+            if (is_file($path)) {
+                /** @psalm-suppress UnresolvableInclude */
+                (require $path)($routes->withPath($path), $this);
+            }
         }
     }
 }
