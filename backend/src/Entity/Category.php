ed31162d63121f1a67e3200fb0590fd64e6ca3f9
Kamil Kokot <kamil@kokot.me>
2021-01-15 22:51:36 +0100

    Add categories & languages endpoints

---
+++ backend/src/Entity/Category.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class Category
+{
+    public int $id;
+
+    public string $name;
+
+    public string $emoji;
+}
---
+++ backend/src/Entity/Language.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class Language
+{
+    public int $id;
+
+    public string $name;
+
+    public string $emoji;
+}
