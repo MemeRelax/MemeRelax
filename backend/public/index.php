1cc0c7104c8e1c0139ca3a28fd5c358f6a236ec8
Kamil Kokot <kamil@kokot.me>
2021-01-15 18:36:57 +0100

    Add an ability to upload files

--- backend/public/index.php
+++ backend/public/index.php
@@ -17,8 +17,8 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-$kernel   = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request  = Request::createFromGlobals();
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
