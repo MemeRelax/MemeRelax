cb94ec00231f88afd8d7ea2b0c7e3e60aa0a73dc
Kamil Kokot <kamil@kokot.me>
2021-01-15 15:19:02 +0100

    Require Psalm for static analysis

--- backend/src/Kernel.php
+++ backend/src/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends BaseKernel
             $container->import('../config/services.yaml');
             $container->import('../config/{services}_'.$this->environment.'.yaml');
         } elseif (is_file($path = \dirname(__DIR__).'/config/services.php')) {
+            /** @psalm-suppress UnresolvableInclude */
             (require $path)($container->withPath($path), $this);
         }
     }
@@ -32,6 +33,7 @@ class Kernel extends BaseKernel
         if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
             $routes->import('../config/routes.yaml');
         } elseif (is_file($path = \dirname(__DIR__).'/config/routes.php')) {
+            /** @psalm-suppress UnresolvableInclude */
             (require $path)($routes->withPath($path), $this);
         }
     }
