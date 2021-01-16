f9c17f2029dbfa3c6f90257ba9e497ee5cffc24b
Kamil Kokot <kamil@kokot.me>
2021-01-16 14:11:36 +0100

    Use ImageRepository instead of EntityManager & automatically flush after event handlers

--- backend/src/Handler/ScanImageContentHandler.php
+++ backend/src/Handler/ScanImageContentHandler.php
@@ -4,21 +4,17 @@ declare(strict_types=1);
 
 namespace App\Handler;
 
-use App\Entity\Image;
 use App\Event\ImageUploaded;
 use App\Ocr\Scanner;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\EntityRepository;
+use App\Repository\ImageRepository;
 use Exception;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
-use function assert;
-
 final class ScanImageContentHandler implements MessageHandlerInterface
 {
     public function __construct(
-        private EntityManagerInterface $entityManager,
+        private ImageRepository $imageRepository,
         private Scanner $scanner,
         private string $uploadDirectory
     ) {
@@ -26,10 +22,7 @@ final class ScanImageContentHandler implements MessageHandlerInterface
 
     public function __invoke(ImageUploaded $event): void
     {
-        $repository = $this->entityManager->getRepository(Image::class);
-        assert($repository instanceof EntityRepository);
-
-        $image = $repository->find($event->getImageId());
+        $image = $this->imageRepository->find($event->getImageId());
 
         if ($image === null) {
             throw new Exception('wtf?');
@@ -38,8 +31,5 @@ final class ScanImageContentHandler implements MessageHandlerInterface
         $scannedContent = $this->scanner->scan(new File($this->uploadDirectory . '/' . $image->getFilePath()));
 
         $image->setScannedContent($scannedContent);
-
-        $this->entityManager->persist($image);
-        $this->entityManager->flush();
     }
 }
