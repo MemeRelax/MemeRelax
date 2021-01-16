024311bba1453069b36d76eb8e4342bb88eaf022
Magda Kokot <magda.kokot@mandogroup.com>
2021-01-16 10:29:26 +0000

    create and route to login component

---
+++ frontend/src/components/TheLoginForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="c-login-form">
+    <div class="c-login-form__container">
+      LOGIN FORM
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.c-login-form {
+  @include pad-tb-spacer(4);
+}
+
+.c-login-form__container {
+  @extend .u-container;
+}
+</style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="v-login">
+    <TheHeader />
+    <div class="v-login__container">
+      <TheLoginForm />
+    </div>
+    <TheFooter />
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "@/components/TheFooter.vue";
+import TheLoginForm from "@/components/TheLoginForm.vue";
+
+export default {
+  name: "Login",
+  components: {
+    TheHeader,
+    TheFooter,
+    TheLoginForm,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.v-login {
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+}
+
+.v-login__container {
+  @extend .u-container-columns;
+}
+</style>
