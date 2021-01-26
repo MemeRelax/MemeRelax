d0c54d332f56fa5708c46de450aeeaef5451592b
Magda Kokot <magda.kokot@mandogroup.com>
2021-01-26 14:04:50 +0000

    create base radio button components

---
+++ frontend/src/components/BaseRadioGroup.vue
@@ -0,0 +1,32 @@
+<template>
+  <BaseRadio
+    v-for="option in options"
+    :key="option.value"
+    :label="option.label"
+    :value="option.value"
+    :name="name"
+    :modelValue="modelValue"
+    @update:modelValue="'update:modelValue', $event"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: [String, Number],
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
